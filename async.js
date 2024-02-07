14d74867382978b8aef8a45dab64c8fc6afba2c5
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-07 21:08:59 +0600

    aldkjflk

--- async.js
+++ async.js
@@ -1,5 +1,25 @@
+function doSomething() {
+  console.log("third");
+}
+
 console.log("first");
 console.log("second");
-console.log("third");
+// setTimeout(() => {
+//   console.log("third");
+// }, 5000);
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => {
+    console.log("response received");
+    return response.json();
+  })
+  .then((json) => console.log(json));
+
 console.log("fourth");
 console.log("fifth");
+
+// github api
+// fetch("https://api.github.com/users/programming-hero1")
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.log(err));
